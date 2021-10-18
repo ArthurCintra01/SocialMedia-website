69237c99ad5cb1371a9af36bce1a1d77d1190c0d
Arthur Cintra <turcko.cintra@gmail.com>
2021-10-18 16:21:05 -0300

    commenting the api

--- network/views.py
+++ network/views.py
@@ -21,73 +21,94 @@ def index(request):
 
 def posts(request, username):
     if username == "all":
+        # getting the posts
         posts = Post.objects.all()
+        # order by newer
         posts = posts.order_by("-timestamp").all()
+        #pagination
         p = Paginator(posts,10)
         page = request.GET.get('page')
         posts = p.get_page(page)
+        # see if user liked the posts
         for post in posts:
             if request.user in post.usersLiked.all():
                 post.liked = True
+        # return posts
         return JsonResponse([post.serialize() for post in posts], safe=False)
     elif username == "following":
         following_posts = []
         user = request.user
+        # getting all posts
         posts = Post.objects.all()
         posts = posts.order_by("-timestamp").all()
+        # getting only posts from users that the current user is following
         for post in posts:
             if post.user in user.following.all():
                 following_posts.append(post)
+        # see if user liked the posts
         for post in following_posts:
             if request.user in post.usersLiked.all():
                 post.liked = True
             post.save()
+        # pagination
         p = Paginator(posts,10)
         page = request.GET.get('page')
         posts = p.get_page(page)
+        # return posts
         return JsonResponse([post.serialize() for post in following_posts], safe=False)
     else:
+        # getting only posts posted by the current user
         postsUser = User.objects.get(username = username)
         posts = Post.objects.filter(user = postsUser).all() 
         posts = posts.order_by("-timestamp").all()
+        # see if the user liked his own posts
         for post in posts:
             if request.user in post.usersLiked.all():
                 post.liked = True
+        # pagination
         p = Paginator(posts,10)
         page = request.GET.get('page')
         posts = p.get_page(page)
+        # return posts
         return JsonResponse([post.serialize() for post in posts], safe=False)
 
 
 @csrf_exempt
 def user(request, username):
+    # get user by it's username
     try:
         user = User.objects.get(username=username)
     except User.DoesNotExist:
         return JsonResponse({"error": "User not found."}, status=404)
 
     if request.method == 'GET':
+        # see if the user is being followed by the current user
         if request.user in user.followers.all():
             user.current_user_follows = True
         else:
             user.current_user_follows = False
-
+        # return the user
         return JsonResponse(user.serialize())
 
     # to follow/ unfollow user
     if request.method == 'PUT':
         current_user = request.user
         user = User.objects.get(username=username)
+        # getting the data from the fetch call
         data = json.loads(request.body)
         if data.get("follow") == True:
+            # checking if the current user is following the user of the page
             if current_user in user.followers.all():
+                # remove from followers if his already following 
                 user.followers.remove(current_user)
                 current_user.following.remove(user)
                 user.current_user_follows = False
             elif current_user not in user.followers.all():
+                # add to followers if his not a follower
                 current_user.following.add(user)
                 user.followers.add(current_user)
                 user.current_user_follows = True
+        # save to database
         current_user.save()
         user.save()
         return HttpResponse(status=204)
@@ -95,7 +116,9 @@ def user(request, username):
 @login_required
 @csrf_exempt
 def profile_page(request, username):
+    # get user
     user = User.objects.get(username=username)
+    # rendering profile page of the user
     return render(request, "network/profilepage.html",{
         "profile_user": user,
         "user": request.user
@@ -124,38 +147,50 @@ def add(request):
 @login_required
 @csrf_exempt
 def post(request, post_id):
+    # getting post by it's id
     try:
         post = Post.objects.get(pk=post_id)
         user = request.user
     except Post.DoesNotExist:
         return JsonResponse({"error": "Post not found."}, status=404)
 
+    # for editing the post
     if request.method == 'POST':
+        # getting the new content of the post
         data = json.loads(request.body)
         post.content = data.get("content")
+        # saving changes to the post
         post.save()
         return HttpResponse(status=204)
 
+    # for liking/unliking a post
     if request.method == 'PUT':
         data = json.loads(request.body)
         if data.get("like") == True:
+            # checking if user liked the post 
             if user in post.usersLiked.all():
+                # removing user from the likes
                 post.usersLiked.remove(user)
                 post.likes = post.likes - 1
                 post.liked = False
             else:
+                # adding user to the likes
                 post.usersLiked.add(user)
                 post.likes = post.likes + 1
                 post.liked = True
+        # saving posts
         post.save()
         return HttpResponse(status=204)
 
     if request.method == 'GET':
+        # checking if current user liked the post
         if user in post.usersLiked.all():
             post.liked = True
         else: 
             post.liked = False
+        # saving changes to the post
         post.save()
+        # returning the post
         return JsonResponse(post.serialize())
 
 
